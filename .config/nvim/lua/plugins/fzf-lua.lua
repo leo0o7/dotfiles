47272604948c59c80e61b89df6c073b7e41d61cf
leo0o7 <giardinoleonardo34@gmail.com>
2026-05-14 13:53:05 +0200

    add keymap to toggle ignored files in fzf-lua

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -1,8 +1,18 @@
 return {
 	{
 		"ibhagwan/fzf-lua",
-		opts = {
-			"borderless-full",
-		},
+		opts = function()
+			local actions = require("fzf-lua.actions")
+
+			return {
+				"borderless-full",
+				actions = {
+					files = {
+						true,
+						["ctrl-g"] = { fn = actions.toggle_ignore, reuse = true, header = false },
+					},
+				},
+			}
+		end,
 	},
 }
