e6f5030a2e08e6cce6e685dde7a0546a3ae0ce86
leo0o7 <giardinoleonardo34@gmail.com>
2026-05-28 20:41:11 +0200

    fix markdown files sometimes not being set to foldmethod=expr

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,6 +15,19 @@ vim.g.clipboard = {
 }
 
 require("config.lazy")
+
+-- HACK: LazyVim's FileType autocmd for treesitter folding is
+-- created too late for the first markdown file opened in a session.
+-- This runs at init time and catches every file.
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	group = vim.api.nvim_create_augroup("markdown_fold_fix", { clear = true }),
+	callback = function()
+		vim.opt_local.foldmethod = "expr"
+		vim.opt_local.foldexpr = "v:lua.LazyVim.treesitter.foldexpr()"
+	end,
+})
+
 require("luasnip.loaders.from_lua").load({
 	paths = { "~/.config/nvim/lua/snippets" },
 })
