1359c696fb4a59189b0e40fb061bd06ac75436be
leo0o7 <giardinoleonardo34@gmail.com>
2026-05-31 15:18:02 +0200

    added diffview plugin

--- .config/nvim/lua/plugins/diff.lua
+++ .config/nvim/lua/plugins/diff.lua
@@ -1,4 +1,33 @@
 return {
-	"axkirillov/unified.nvim",
-	opts = {},
+	"sindrets/diffview.nvim",
+	cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles", "DiffviewFileHistory" },
+	keys = {
+		{ "<leader>gD", "<cmd>DiffviewOpen<cr>", desc = "Diffview: open" },
+		{ "<leader>gH", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview: file history" },
+	},
+	opts = {
+		enhanced_diff_hl = true,
+		use_icons = true,
+		keymaps = {
+			disable_defaults = false,
+			view = {
+				["<leader>e"] = false,
+				{ "n", "<leader>E", "<cmd>DiffviewFocusFiles<cr>", { desc = "Focus file panel" } },
+				{ "n", "<leader>q", "<cmd>DiffviewClose<cr>", { desc = "Close diffview" } },
+			},
+			file_history_panel = {
+				["<leader>e"] = false,
+				{ "n", "<leader>E", "<cmd>DiffviewFocusFiles<cr>", { desc = "Focus file panel" } },
+				{ "n", "<leader>q", "<cmd>DiffviewClose<cr>", { desc = "Close diffview" } },
+			},
+			file_panel = {
+				["<leader>e"] = false,
+				{ "n", "<leader>E", "<cmd>DiffviewFocusFiles<cr>", { desc = "Focus file panel" } },
+				{ "n", "<leader>q", "<cmd>DiffviewClose<cr>", { desc = "Close diffview" } },
+				{ "n", "s", false }, -- disable stage entry
+				{ "n", "S", false }, -- disable stage all
+				{ "n", "U", false }, -- disable unstage all
+			},
+		},
+	},
 }
