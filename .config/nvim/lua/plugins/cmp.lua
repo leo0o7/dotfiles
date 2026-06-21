e73e2f70539fa506f774a5f0c797adb1e24ae931
leo0o7 <giardinoleonardo34@gmail.com>
2026-06-21 10:19:32 +0200

    nvim: clean up cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,67 +1,19 @@
 return {
-	"saghen/blink.cmp",
-	opts = {
-		appearance = {
-			-- sets the fallback highlight groups to nvim-cmp's highlight groups
-			-- useful for when your theme doesn't support blink.cmp
-			-- will be removed in a future release, assuming themes add support
-			use_nvim_cmp_as_default = false,
-			-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- adjusts spacing to ensure icons are aligned
-			nerd_font_variant = "mono",
-		},
-		completion = {
-			accept = {
-				-- experimental auto-brackets support
-				auto_brackets = {
-					enabled = true,
-				},
-			},
-			menu = {
-				draw = {
-					treesitter = { "lsp" },
-				},
-			},
-			documentation = {
-				auto_show = true,
-				auto_show_delay_ms = 200,
-			},
-			ghost_text = {
-				enabled = vim.g.ai_cmp,
-			},
-		},
-
-		-- experimental signature help support
-		-- signature = { enabled = true },
-
-		sources = {
-			-- adding any nvim-cmp sources here will enable them
-			-- with blink.compat
-			compat = {},
-			default = { "lsp", "path", "snippets", "buffer", "filemention" },
-			providers = {
-				filemention = {
-					name = "filemention",
-					module = "filemention.sources.blink",
-				},
-			},
-		},
-
-		cmdline = {
-			enabled = true,
-		},
-
-		keymap = {
-			preset = "enter",
-			["<C-y>"] = { "select_and_accept" },
-			["<Tab>"] = {
-				"select_next",
-				"fallback",
-			},
-			["<S-Tab>"] = {
-				"select_prev",
-				"fallback",
-			},
-		},
-	},
+  "saghen/blink.cmp",
+  opts = {
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer", "filemention" },
+      providers = {
+        filemention = {
+          name = "filemention",
+          module = "filemention.sources.blink",
+        },
+      },
+    },
+    keymap = {
+      ["<C-y>"] = { "select_and_accept" },
+      ["<Tab>"] = { "select_next", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+    },
+  },
 }
