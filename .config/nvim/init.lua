147da35b8d54b678ce8e9f47ddbf8af899331173
leo0o7 <giardinoleonardo34@gmail.com>
2026-06-22 10:02:33 +0200

    trying out ghostty

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,5 @@
 -- force set language to english
 -- vim.api.nvim_exec("language en_US", true)
-
--- set OSC 52 clipboard to avoid encoding issues on mac
-vim.g.clipboard = {
-	name = "OSC 52",
-	copy = {
-		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-	},
-	paste = {
-		["+"] = { "pbpaste" },
-		["*"] = { "pbpaste" },
-	},
-}
-
 require("config.lazy")
 
 -- HACK: LazyVim's FileType autocmd for treesitter folding is
